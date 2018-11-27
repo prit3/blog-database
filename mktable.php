5f8f0c81f3d43edc4f21467409d6e6c4094aa923
prit3 <44801207+prit3@users.noreply.github.com>
2018-11-27 12:58:11 +0100

    mktable edit

--- mktable.php
+++ mktable.php
@@ -2,7 +2,7 @@
 include ('dbconn.php');
 
 $mktable = "CREATE TABLE BlogPosts(
-			id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+			id INT(11) AUTO_INCREMENT PRIMARY KEY,
 			Naam VARCHAR(50) NOT NULL,
 			Title VARCHAR(50) NOT NULL,
 			Blogtext TEXT NOT NULL,
